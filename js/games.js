9e3e8cd8ad76cba043997bb16343d5678ec02c53
Finn Kairies <finn.kairies@gmail.com>
2020-03-23 13:16:32 +0100

    improve circle game

--- js/games.js
+++ js/games.js
@@ -1,26 +1,31 @@
 function spawnDivs(params) {
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 100; i++) {
     var div = document.createElement("div");
+    var p = document.createElement("p");
 
     // Create the text node for anchor element.
-    var text = document.createTextNode(i);
+    var text = document.createTextNode(i + 1);
 
     // Append the text node to anchor element.
-    div.appendChild(text);
+    p.appendChild(text);
+    div.appendChild(p);
 
     // Set the title.
     div.classList.add("circle");
 
     // Append the anchor element to the body.
 
-    div.style.top = Math.random() * 200 + "px";
-    div.style.left = Math.random() * 200 + "px";
+    div.style.top = Math.random() * 40 + "vhs";
+    div.style.left = Math.random() * 100 + "%";
     circleGame = document.querySelector(".circle-game");
     circleGame.appendChild(div);
   }
   circleGame.addEventListener("click", e => {
     if (circleGame.children[0] === e.target) {
       e.target.remove();
+    }
+    if (circleGame.children[0].children[0] === e.target) {
+      e.target.parentNode.remove();
     }
   });
 }
